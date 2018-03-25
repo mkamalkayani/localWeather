860eb7a92052705e6cad1848d13ac16a412f24f0
Muhammad Kamal <mkamalkayani@gmail.com>
2018-03-25 19:51:05 +0200

    Fetch api working

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
 var userLocation = document.querySelector("p.user_location");
+var weatherDescription = document.querySelector("p.weather_description");
 
-if(!navigator.geolocation){
+/*if(!navigator.geolocation){
 	console.log("Geolocation not supported by the browser");}
+	else {
+		navigator.geolocation.getCurrentPosition(function(position){
+		var lat = position.coords.latitude;
+		var lon = position.coords.longitude;
+		})};*/
+	
+lat = 54.35;
+lon = 13.05;
 
-else {
-	navigator.geolocation.getCurrentPosition(function(position){
-	var latitude = position.coords.latitude;
-	var longitude = position.coords.longitude;
+//Weather api
+var URL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&" + "lon=" + lon +"&APPID=5f99e28b1a3c92cec71c5ca344fccde7";
 
-var URL = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + latitude + "," + longitude
- + "&result_type=locality&key=AIzaSyDXPOmVwTKelJB5cuh0T_Hwac8kuG7UWzo";
-
-
-	fetch(URL)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(myJson) {
-  	userLocation.innerHTML = myJson.results[0].formatted_address;
-  });
-
-})};
-
+fetch(URL)
+	.then(function(response){
+		return response.json();
+	})
+	.then(function(weatherData){
+		console.log(weatherData)
+		userLocation.innerHTML = weatherData.name;
+	});
