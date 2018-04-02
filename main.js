409596dcf94851202cf3e89bebe645eb466688a7
Muhammad Kamal <mkamalkayani@gmail.com>
2018-04-02 21:06:03 +0200

    "Used flexbox to create the desired layout"

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-var userLocation = document.querySelector(".user_location");
-var weatherDescription = document.querySelector(".weather_description");
-var windSpeed = document.querySelector(".wind_speed");
-var weatherIcon = document.querySelector("img.weather_icon");
+var userLocation = document.querySelector(".user-location");
+var weatherDescription = document.querySelector(".weather-description");
+var windSpeed = document.querySelector(".wind-speed");
+var windDirection = 0;
+var weatherIcon = document.querySelector("img.weather-icon");
 var temp = document.querySelector(".temperature");
 
 /*if(!navigator.geolocation){
@@ -16,18 +17,19 @@ lat = 54.35;
 lon = 13.05;
 
 //Weather api
-var URL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&" + "lon=" + lon +"&APPID=5f99e28b1a3c92cec71c5ca344fccde7";
+var URL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&" + "lon=" + lon +"&units=metric&APPID=5f99e28b1a3c92cec71c5ca344fccde7";
 
 fetch(URL)
 	.then(function(response){
 		return response.json();
 	})
 	.then(function(weatherData){
-		//console.log(weatherData)
-		userLocation.innerHTML = weatherData.name;
+		console.log(weatherData)
+		userLocation.innerHTML = weatherData.name + ", " + weatherData.sys.country;
 		weatherDescription.innerHTML = weatherData.weather["0"].description;
-		windSpeed.innerHTML = Math.floor(weatherData.wind.deg) + " " + weatherData.wind.speed;
-		temp.innerHTML = Math.floor((weatherData.main.temp - 273.0)) + " &#8451";
+		windDirection = Math.floor(weatherData.wind.deg);
+		windSpeed.innerHTML = "SW" + " " + weatherData.wind.speed + " knots";
+		temp.innerHTML = Math.floor(weatherData.main.temp) + " &#8451";
 
 		var weatherIconCode = weatherData.weather["0"].icon;
 		weatherIcon.src = "http://openweathermap.org/img/w/" + weatherIconCode + ".png";
