d27233a8f83e374e12bfbb0c1e1d952237f7c723
Muhammad Kamal <mkamalkayani@gmail.com>
2018-04-28 18:19:40 +0200

    changed default location

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 $(function(){
+	//Default user coordinates
+	var userCordinates = [52.5,13.4];
+	weatherUpdate(userCordinates);
+	currentLocation();
 
-var userCordinates = [54.35,13.05];
-var URL = createURL(userCordinates);
-fetchData(URL);
-
-var searchBar = document.getElementById("search-bar"); 
-
+var searchBar = document.getElementById("search-bar");
 //Event to search the weather data for the entered address
 searchBar.addEventListener('change',handleSearch);
 
@@ -13,6 +12,29 @@ searchBar.addEventListener('change',handleSearch);
 searchBar.addEventListener('click',function(){
 	$("#search-bar").val("");
 });
+
+function weatherUpdate(userCordinates){
+	var URL = createURL(userCordinates);
+	fetchData(URL);
+}
+
+function currentLocation(){
+	return new Promise(function(resolve,reject){
+		if(!navigator.geolocation){
+			reject("Geolocation not supported by the browser");
+		}
+		else {
+			navigator.geolocation.getCurrentPosition(function(position){
+				var lat = position.coords.latitude;
+				var lon = position.coords.longitude;
+				resolve([lat,lon]);
+			})				
+		}
+	})
+	.then(function(userCordinates){
+		weatherUpdate(userCordinates);
+	});
+}
 
 //Fetches weather data and updates the webpage
 function fetchData(URL){
@@ -45,20 +67,6 @@ function fetchData(URL){
 	})
 }
 
-//Retrieves user's Current Geolocation
-function currentlocation(){
-	if(!navigator.geolocation){
-		console.log("Geolocation not supported by the browser");
-	}
-	else {
-		navigator.geolocation.getCurrentPosition(function(position){
-			var lat = position.coords.latitude;
-			var lon = position.coords.longitude;
-		})
-		return [lat,lon];
-	}
-}
-
 //Creates weather api URL
 function createURL([lat,lon]){
 	var URL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&" + "lon=" + lon +"&units=metric&APPID=5f99e28b1a3c92cec71c5ca344fccde7";
@@ -75,22 +83,20 @@ function degToCompass(num){
 //Function to handle change event on searh bar
 function handleSearch(){
 	return new Promise(function(resolve,reject){
-	var geocoder = new google.maps.Geocoder();
-	var address = document.getElementById('search-bar').value;
+		var geocoder = new google.maps.Geocoder();
+		var address = document.getElementById('search-bar').value;
 
-    	geocoder.geocode({ 'address': address }, function (results, status) {
+		geocoder.geocode({ 'address': address }, function (results, status) {
 
-	        if (status == google.maps.GeocoderStatus.OK) {
-	            latitude = results[0].geometry.location.lat();
-	            longitude = results[0].geometry.location.lng();
-	            resolve([latitude,longitude])
-	    	}
+			if (status == google.maps.GeocoderStatus.OK) {
+				latitude = results[0].geometry.location.lat();
+				longitude = results[0].geometry.location.lng();
+				resolve([latitude,longitude])
+			}
 		})} 
-	)
+		)
 	.then(coordinates => createURL(coordinates))
 	.then(URL => fetchData(URL))
 }
-
-
 
 })
